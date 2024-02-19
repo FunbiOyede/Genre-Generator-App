7f30ce3d91cc350467110153bed1218c4d5fefa7
SamsonOyede <oyedetobi49@gmail.com>
2024-02-19 21:06:18 +0000

    implement web scrapper

---
+++ Lofi/Models/genreModel.cs
@@ -0,0 +1,5 @@
+
+public class Genre
+{
+    public string Name { get; set; } = string.Empty;
+}
---
+++ Lofi/Scrapper/Engine.cs
@@ -0,0 +1,21 @@
+
+class Engine
+{
+    public Engine()
+    {
+        
+    }
+
+
+
+    private void configureEngine(string url){
+
+        //   var web = new HtmlWeb();
+        //   var webDocument = web.Load(url);
+
+    }
+
+    public void QueryData(){
+
+    }
+}
